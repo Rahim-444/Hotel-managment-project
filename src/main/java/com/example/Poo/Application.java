37f62724d24ab4162e2cccb78fefd5d59649feb2
Rahim <mac@MacBook-Air-de-mac.local>
2024-02-21 22:31:48 +0100

    init porject

--- src/main/java/com/example/Poo/Application.java
+++ src/main/java/com/example/Poo/Application.java
@@ -2,8 +2,12 @@
 
 package main.java.com.example.Poo;
 
+import main.java.com.*;
+import main.java.com.example.Poo.model.Product;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Product product = new Product("TV", 1000.0, 10);
+        System.out.println(product.getName());
     }
 }
