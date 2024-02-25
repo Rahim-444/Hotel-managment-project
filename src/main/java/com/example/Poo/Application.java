bdb6e1da9310092c6cceb785398689b638f0133a
Rahim-444 <abderrahimbelkacemi33@gmail.com>
2024-02-25 16:56:59 +0100

    set back to defualt

--- src/main/java/com/example/Poo/Application.java
+++ src/main/java/com/example/Poo/Application.java
@@ -1,13 +1,7 @@
-//print hello worold
-
 package main.java.com.example.Poo;
-
-import main.java.com.*;
-import main.java.com.example.Poo.model.Product;
 
 public class Application {
     public static void main(String[] args) {
-        Product product = new Product("TV", 1000.0, 10);
-        System.out.println(product.getName());
+        new Login();
     }
 }
---
+++ src/main/java/com/example/Poo/Login.java
@@ -0,0 +1,16 @@
+package main.java.com.example.Poo;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+    private JPanel MainPanel;
+
+    public Login() {
+        super("Login");
+        setSize(1280, 720);
+        setContentPane(MainPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+}
