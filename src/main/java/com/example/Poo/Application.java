780335c68dc66c14e6e35cd0cec0f8338fecdab7
Rahim-444 <abderrahimbelkacemi33@gmail.com>
2024-02-25 17:27:04 +0100

    set back to defualt

--- src/main/java/com/example/Poo/Application.java
+++ src/main/java/com/example/Poo/Application.java
@@ -2,6 +2,6 @@ package main.java.com.example.Poo;
 
 public class Application {
     public static void main(String[] args) {
-        new Login();
+        System.out.println("Hello World");
     }
 }
