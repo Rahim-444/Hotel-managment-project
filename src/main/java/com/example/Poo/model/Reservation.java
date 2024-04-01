9993aeda2d6e8def2cef37ccd0266d1ba053d278
AmineDana <danaamine080807@gmail.com>
2024-04-01 11:35:43 -0400

    modal and 2 controller functions (get/post)

---
+++ src/main/java/com/example/Poo/model/Reservation.java
@@ -0,0 +1,61 @@
+package main.java.com.example.Poo.model;
+
+import java.time.LocalDataTime;
+
+
+public class Reservation {
+  private int id;
+  private int roomId;
+  private int userId;
+  private LocalDataTime startTime;
+  private LocalDateTime endTime;
+
+  // Constructor
+    public Reservation(int id, int roomId, int userId, LocalDateTime startTime, LocalDateTime endTime) {
+        this.id = id;
+        this.roomId = roomId;
+        this.userId = userId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+  public int getId(){
+    return this.id;
+  }
+
+  public void setId(int id){
+    this.id = id;
+  }
+
+  public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+}
