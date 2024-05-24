9de46612294c09a201977cbc82daea3bfeafb1e5
AmineDana <danaamine080807@gmail.com>
2024-05-24 18:08:08 -0400

    mailing system

---
+++ src/main/java/com/example/Poo/controller/SendEmail.java
@@ -0,0 +1,63 @@
+package main.java.com.example.Poo.controller;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.*;
+import java.io.IOException;
+import java.util.Properties;
+
+
+public class SendEmail
+{
+
+    Session newSession = null;
+    MimeMessage mimeMessage = null;
+    
+    public boolean sendEmail(String Email) {
+        String fromUser = "danaamine080807@gmail.com";
+        String fromUserPassword = "pasword";
+        String emailHost = "smtp.gmail.com";
+        try {
+            Transport transport = newSession.getTransport("smtp");
+            transport.connect(emailHost, fromUser, fromUserPassword);
+            transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+            transport.close();
+            System.out.println("Email successfully sent!!!");
+            return true; // Return true if the email was sent successfully
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            System.out.println("Failed to send email");
+            return false; // Return false if there was an error sending the email
+        }
+    }
+
+    public MimeMessage draftEmail(String Email , String message , String token) throws AddressException, MessagingException, IOException {
+        String[] emailReceipients = {"hermezabderrahim568@gmail.com",Email};  //Enter list of email recepients
+        String emailSubject = "sheraton hotel token";
+        String emailBody = message + token + " " + emailReceipients[1];
+        mimeMessage = new MimeMessage(newSession);
+
+        for (int i =0 ;i<emailReceipients.length;i++)
+        {
+            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(emailReceipients[i]));
+        }
+        mimeMessage.setSubject(emailSubject);
+
+        MimeBodyPart bodyPart = new MimeBodyPart();
+        bodyPart.setContent(emailBody,"text/html"); // Set the correct MIME type
+        MimeMultipart multiPart = new MimeMultipart();
+        multiPart.addBodyPart(bodyPart);
+        mimeMessage.setContent(multiPart);
+        return mimeMessage;
+    }
+
+    public void setupServerProperties() {
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        newSession = Session.getDefaultInstance(properties,null);
+    }
+
+}
